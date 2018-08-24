fdc55c04999fdbcbaa46baafacc081684290e698
Kevin Dang <overcyn@gmail.com>
2018-08-23 23:32:39 -0700

    Initial Commit

---
+++ KeynoteOverlay/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KeynoteOverlay
+//
+//  Created by Kevin Dang on 8/23/18.
+//  Copyright © 2018 Matcha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
